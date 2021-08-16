dc05c8386b64821c83cfac9299f55caee323b029
nic-lloyd <61630547+nic-lloyd@users.noreply.github.com>
2021-08-16 14:59:48 -0500

    Init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ReactTypingEffect from "react-typing-effect";
+import { Card, Button, Spinner } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Whats up motherfucker
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Card style={{ width: "18rem" }}>
+          <Card.Body>
+            <Card.Img variant='top' src='https://avatars.githubusercontent.com/u/61630547?v=4'/>
+            <Card.Title>Nicolas Lloyd</Card.Title>
+            <Card.Text>Web Dev Extraordinaire</Card.Text>
+          </Card.Body>
+        </Card>
       </header>
     </div>
   );
